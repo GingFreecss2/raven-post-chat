b6171887a9b8a8607d6e6e7a38c66068b8beb2ca
LAASRI Mohamed <84083555+GingFreecss2@users.noreply.github.com>
2023-10-29 06:47:29 +0100

    project ready for deployement

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,19 @@
-import { Redis } from '@upstash/redis'
+// import { Redis } from '@upstash/redis'
+
+// export const db = new Redis({
+//   url: process.env.UPSTASH_REDIS_REST_URL,
+//   token: process.env.UPSTASH_REDIS_REST_TOKEN,
+// })
+import { Redis } from '@upstash/redis';
+
+const redisUrl = process.env.UPSTASH_REDIS_REST_URL;
+const redisToken = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+if (!redisUrl || !redisToken) {
+    throw new Error('UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN environment variables are required.');
+}
 
 export const db = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-})
+    url: redisUrl,
+    token: redisToken,
+});
